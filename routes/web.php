e3bffd983e43c8befc8a0ffff9c7fa32f01f1b7a
Farhad Hossain/OSS SComm <farhad.hossain@summitcommunications.net>
2024-08-13 13:46:34 +0600

    Redis configuration done and working

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return 'test';
     return view('welcome');
 });
 
@@ -14,24 +15,29 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::resource('posts', App\Http\Controllers\PostController::class);
 
-Route::get('/test-elasticsearch', function (ElasticsearchService $elasticsearchService) {
-    $response = $elasticsearchService->search('test_index', '_doc', [
-        'query' => [
-            'match_all' => new stdClass()
-        ]
-    ]);
+// Route::get('/test-elasticsearch', function (ElasticsearchService $elasticsearchService) {
+//     $response = $elasticsearchService->search('test_index', '_doc', [
+//         'query' => [
+//             'match_all' => new stdClass()
+//         ]
+//     ]);
 
-    return response()->json($response);
-});
+//     return response()->json($response);
+// });
 
 Route::get('/test-redis', function () {
     $redis = Redis::connection();
+    $testArray = ["name"=>"Farhad", "Role"=>"Admin"];
 
     // Set a value
-    $redis->set('test_key', 'Redis is working!');
+    // $redis->set('test_key', json_encode($testArray));
+    // $redis->set('test', "abc");
 
     // Get the value
-    $value = $redis->get('test_key');
+    // return 'set key';
+    // $value = $redis->get('test');
+    $value = $redis->get('test');
+    dd($value);
 
     return $value;
 });
